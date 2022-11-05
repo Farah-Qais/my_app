60b4468903a0a3fad4a45f4487ccbd281193cd63
Farah <115896635+Farah-Qais@users.noreply.github.com>
2022-11-05 16:10:02 +0300

    Added an update to the house model

--- app/models/hose.rb
+++ app/models/hose.rb
@@ -1,10 +1,4 @@
 class Hose < ApplicationRecord
-    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }  
-    validates :username, length: {minimum: 3}, allow_blank: false 
-    validates :name, presence: true, uniqueness:{case_sensitive:false}  
-    validates :type, length: {minimum: 3}, allow_blank: false 
-    validates :size, length: {minimum: 3}, allow_blank: false
-    has_one :Profile 
-    has_many :books
-    belongs_to :user
+    validates :price, :presence =>true,length: {minimum: 3}
+    validates :size, :presence =>true,length: {minimum: 3}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,3 @@
-class User < ApplicationRecord
-    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }  
-    validates :username, length: {minimum: 3}, allow_blank: false 
-    validates :name, presence: true, uniqueness:{case_sensitive:false}  
-    validates :type, length: {minimum: 3}, allow_blank: false 
-    has_one :Profile 
-    has_many :books
-    has_one :hose
+class User < ApplicationRecord  
+    has_one :house
 end
